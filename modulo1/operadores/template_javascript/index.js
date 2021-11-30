7c6510a2c7f768de3dad877899f11b3751729439
Vanessa Dias <programmer.vanessa@gmail.com>
2021-11-30 16:23:40 -0300

    Atividade Opeadores

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,45 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGOS
+
+
+// 1 
+/*
+a) V && F = FALSO
+b) V && F && V = FALSO
+c) V && V = VERDADEIRO
+d) Boolean
+*/
+
+// 2
+/* A operação não foi considereada uma soma, mas sim uma concatenação por conta da função prompt() que é 
+sempre uma string.
+Para ser considerado uma soma a variavel precisa ser identificada como "tipo número"
+EXEMPLO: const soma = Number(primeiroNumero) + Number(segundoNumero)
+ */
+
+
+// 3
+/* 
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = Number(primeiroNumero) + Number(segundoNumero)
+
+console.log(soma)
+*/
+
+// EXECERCÍCIOS DE ESCRITA DE CÓDIGOS
+
+// 1
+
+const idade = Number(prompt("Quantos anos você tem?"));
+const amigo = Number(prompt("Quantos anos tem o seu (sua) melhor amigo (a)?"));
+
+const MaisVelho = idade >= amigo;
+
+const 
+const.log("Sua idade é maior que a do seu (sua) amigo (a)?", MaisVelho)
+
+//2
+
+// 3
+
