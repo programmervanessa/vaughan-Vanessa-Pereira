82d7ea5a8f5a6930967143cd563db01a718b00b1
Vanessa Dias <programmer.vanessa@gmail.com>
2021-12-01 16:42:32 -0300

    Exercícios arrays e strings - incompleto -

---
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,27 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+/*
+
+//1
+a. Indefenido (não foi especificado um array)
+b. null
+c. 11
+d. 3 ( i = posição 0 que é o número 3 no array)
+e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13] aqui o comando foi: no array posição 0 + 1 incluir 19, 
+então, posição 0+1 = posição 1 que é o número 4 que foi substituido pelo numero 19
+f. 9 (Por se tratar de uma variável o comando foi: somar a posição "i"= posição "0" que é o número 3 + 6 = 9)
+
+
+//2
+SUBI NUM ÔNIBUS EM MIRROCOS (todas as letras foram colocadas em maiúsculo     e todas as letras "a" foram 
+substituídas pela letra "i")
+
+
+*/
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO
+
+//1
+const email = prompt("Digite o seu e-mail");
+const mensagem = `O e-mail do usuário ${email} foi cadastrado com sucesso! Seja bem vindo (a)`
+
+console.log(mensagem)
