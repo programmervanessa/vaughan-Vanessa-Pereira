ddbe5ff4aad3ed1b101a2515150d04a5c438d0af
Vanessa Dias <programmer.vanessa@gmail.com>
2021-12-07 17:23:19 -0300

    condicionais-incompleto

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,18 @@
+// Exercícios de interpretação de código //
+/*
+ 1.a) O código realiza o teste de números divisiveis por 2. 
+   b) Se o número que o usuário digitar for par imprime "Passou no teste". 
+   c) Se o número digitado pelo usuário for ímpar imprime "Não passou no teste".
+
+ 2. a) O código serve para retornar o valor da fruta digitada pelo usuário.
+    b) O preço da fruta mação é R$ 2.25  
+    c) O preço da fruta Pêra é R$ 5.5  5
+
+ 3. a) A primeira linha está pedindo para o usuário digitar um número.
+    b) Digitando 10 a mensagem seria: Esse número passou no teste".
+       Digitando -10 não havaria nenhuma mensagem
+    c) Não haverá retorno no console, pois a função nao foi chamada.   
+
+*/  
+
+
